327480684f444e8a4bff491ef971ed32b36c81ce
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 14:10:22 +0100

    Load principal teacher module styles

--- web/app/principal/layout.tsx
+++ web/app/principal/layout.tsx
@@ -1,5 +1,6 @@
 import "./principal.css";
 import "./principal-approvals.css";
+import "./principal-teachers.css";
 
 export default function PrincipalLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
