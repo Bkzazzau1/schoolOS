702185c52670f42d0cccc75ed4a94999bfa2eb6f
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 15:40:04 +0100

    Wire Primary profile workspace styles

--- web/app/headmaster/layout.tsx
+++ web/app/headmaster/layout.tsx
@@ -10,6 +10,7 @@ import "./headmaster-communication.css";
 import "./headmaster-incidents.css";
 import "./headmaster-ai.css";
 import "./headmaster-performance.css";
+import "./headmaster-profile.css";
 
 export default function HeadmasterLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
