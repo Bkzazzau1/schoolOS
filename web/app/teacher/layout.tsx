997a3b5df8ea67dd122e4ede977b0ef876a956f6
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-14 00:59:43 +0100

    Load CBT styles in teacher portal

--- web/app/teacher/layout.tsx
+++ web/app/teacher/layout.tsx
@@ -2,6 +2,7 @@ import "./teacher.css";
 import "./module-pages.css";
 import "./reports.css";
 import "./weekly-progress/weekly-progress.css";
+import "./cbt/cbt.css";
 
 export default function TeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
