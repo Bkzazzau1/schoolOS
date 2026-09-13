d95f8a56ed4eca700c98167f46702f207ccd3b50
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 12:48:45 +0100

    Load final teacher module styles

--- web/app/teacher/layout.tsx
+++ web/app/teacher/layout.tsx
@@ -1,5 +1,6 @@
 import "./teacher.css";
 import "./module-pages.css";
+import "./teacher-final.css";
 
 export default function TeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
