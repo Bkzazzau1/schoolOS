15f569d97caa949a05462fe3fd18c366fcc6d686
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 15:18:53 +0100

    Wire Primary academics styles into Headmaster layout

--- web/app/headmaster/layout.tsx
+++ web/app/headmaster/layout.tsx
@@ -2,6 +2,7 @@ import "./headmaster.css";
 import "./headmaster-assignments.css";
 import "./headmaster-teachers.css";
 import "./headmaster-pupils.css";
+import "./headmaster-academics.css";
 
 export default function HeadmasterLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
