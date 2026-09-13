3b97457a954ea0e13efbdc74c3c293ae017ba011
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 14:25:14 +0100

    Load principal results styles

--- web/app/principal/layout.tsx
+++ web/app/principal/layout.tsx
@@ -4,6 +4,7 @@ import "./principal-teachers.css";
 import "./principal-academics.css";
 import "./principal-students.css";
 import "./principal-attendance.css";
+import "./principal-results.css";
 
 export default function PrincipalLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
