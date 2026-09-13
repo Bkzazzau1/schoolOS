20c8059efaa830bee0adcf027d3a386d26d06dbc
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-14 00:07:28 +0100

    Add Parent Learning Progress route

---
+++ web/app/parent/progress/page.tsx
@@ -0,0 +1,5 @@
+import StudentIntelligenceView from "../../../components/student-intelligence-view";
+
+export default function ParentLearningProgressPage(){
+  return <StudentIntelligenceView mode="parent"/>;
+}
