37c568b6541df3a221cdd9d222616d1d40a0d468
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-14 00:12:30 +0100

    Load fee reminder center styles

--- web/app/finance-office/layout.tsx
+++ web/app/finance-office/layout.tsx
@@ -2,6 +2,7 @@ import "./finance-office.css";
 import "./smart-collections.css";
 import "./revenue-assurance.css";
 import "./store.css";
+import "./reminders.css";
 import FinanceOfficeShell from "../../components/finance-office-shell";
 
 export default function FinanceOfficeLayout({children}:{children:React.ReactNode}){
