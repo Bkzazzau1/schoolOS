86cf4c10b34056b2bb629f94339828c44656da1d
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 16:14:27 +0100

    Wire Early Years Head Teacher AI styles

--- web/app/headteacher/layout.tsx
+++ web/app/headteacher/layout.tsx
@@ -9,6 +9,7 @@ import "./headteacher-reports.css";
 import "./headteacher-routines.css";
 import "./headteacher-guardians.css";
 import "./headteacher-incidents.css";
+import "./headteacher-ai.css";
 
 export default function HeadTeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
