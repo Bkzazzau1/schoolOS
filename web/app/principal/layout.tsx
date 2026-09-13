1e9a305a3ab0d395a1d94b4919fc141062863c77
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 14:18:27 +0100

    Wire principal attendance styles

--- web/app/principal/layout.tsx
+++ web/app/principal/layout.tsx
@@ -3,6 +3,7 @@ import "./principal-approvals.css";
 import "./principal-teachers.css";
 import "./principal-academics.css";
 import "./principal-students.css";
+import "./principal-attendance.css";
 
 export default function PrincipalLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
