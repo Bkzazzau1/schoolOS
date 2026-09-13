77722f19b01397b47141fff8091b8ec13a631547
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 15:54:49 +0100

    Wire Early Years development styles

--- web/app/headteacher/layout.tsx
+++ web/app/headteacher/layout.tsx
@@ -1,6 +1,7 @@
 import "./headteacher.css";
 import "./headteacher-educators.css";
 import "./headteacher-children.css";
+import "./headteacher-development.css";
 
 export default function HeadTeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
