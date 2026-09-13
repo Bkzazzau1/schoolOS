98ad4ee9cf8a663ee40e7d74bf5233f9466cf34d
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 19:05:51 +0100

    Load proprietor executive module styles

--- web/app/proprietor/layout.tsx
+++ web/app/proprietor/layout.tsx
@@ -1,5 +1,6 @@
 import "./proprietor.css";
 import "./executive.css";
+import "./owner-modules.css";
 
 export default function ProprietorLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
