fa9f7e93a80463f6b0cc4a5a6d49272ba55643a8
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 16:05:22 +0100

    Wire Early Years reports styles

--- web/app/headteacher/layout.tsx
+++ web/app/headteacher/layout.tsx
@@ -5,6 +5,7 @@ import "./headteacher-development.css";
 import "./headteacher-attendance.css";
 import "./headteacher-observations.css";
 import "./headteacher-planning.css";
+import "./headteacher-reports.css";
 
 export default function HeadTeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
