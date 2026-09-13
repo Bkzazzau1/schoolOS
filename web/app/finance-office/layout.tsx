1a7e12a1d03e8e833aada471489f2faa0864e2af
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 22:43:17 +0100

    Load smart collections styles

--- web/app/finance-office/layout.tsx
+++ web/app/finance-office/layout.tsx
@@ -1,4 +1,5 @@
 import "./finance-office.css";
+import "./smart-collections.css";
 import FinanceOfficeShell from "../../components/finance-office-shell";
 
 export default function FinanceOfficeLayout({children}:{children:React.ReactNode}){
