2c7a007cfb492cf05c4ec9079fe8c0fdd1f3a850
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-14 00:51:04 +0100

    Load weekly learning styles

--- web/app/parent/layout.tsx
+++ web/app/parent/layout.tsx
@@ -1,6 +1,7 @@
 import "./parent.css";
 import "./parent-modules.css";
 import "./parent-forum.css";
+import "./weekly-learning/weekly-learning.css";
 import ParentShell from "../../components/parent-shell";
 
 export default function ParentLayout({ children }: Readonly<{ children: React.ReactNode }>) {
