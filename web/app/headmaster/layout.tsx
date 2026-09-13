f87a5c7b48451a9d2b8c0098ff51ad85e1d26cf2
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 15:11:27 +0100

    Wire Primary assignment styles

--- web/app/headmaster/layout.tsx
+++ web/app/headmaster/layout.tsx
@@ -1,4 +1,5 @@
 import "./headmaster.css";
+import "./headmaster-assignments.css";
 
 export default function HeadmasterLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
