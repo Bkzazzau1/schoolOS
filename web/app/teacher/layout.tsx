a59c3a541f09ff937ef1dbfd0e7bf2ff3be44f99
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-14 00:50:07 +0100

    Load weekly learning progress styles

--- web/app/teacher/layout.tsx
+++ web/app/teacher/layout.tsx
@@ -1,6 +1,7 @@
 import "./teacher.css";
 import "./module-pages.css";
 import "./reports.css";
+import "./weekly-progress/weekly-progress.css";
 
 export default function TeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
