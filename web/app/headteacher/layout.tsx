c9b8d331686ecf21d81b3068e9c40be9bb7f548f
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 15:57:17 +0100

    Wire Early Years attendance styles

--- web/app/headteacher/layout.tsx
+++ web/app/headteacher/layout.tsx
@@ -2,6 +2,7 @@ import "./headteacher.css";
 import "./headteacher-educators.css";
 import "./headteacher-children.css";
 import "./headteacher-development.css";
+import "./headteacher-attendance.css";
 
 export default function HeadTeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
