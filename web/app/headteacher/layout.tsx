161a7aaf5b0a8fdb2734d7ad7963b66da26dbde1
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 15:45:40 +0100

    Load Early Years educator styles

--- web/app/headteacher/layout.tsx
+++ web/app/headteacher/layout.tsx
@@ -1,4 +1,5 @@
 import "./headteacher.css";
+import "./headteacher-educators.css";
 
 export default function HeadTeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
