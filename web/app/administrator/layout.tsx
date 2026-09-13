b124d93c2fa2eeee51731c0a014b7eb1670dd199
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-14 00:32:43 +0100

    Load staff attendance styles

--- web/app/administrator/layout.tsx
+++ web/app/administrator/layout.tsx
@@ -1,5 +1,6 @@
 import "./administrator.css";
 import "./attendance/hardware.css";
+import "./staff-attendance/staff-attendance.css";
 import AdministratorShell from "../../components/administrator-shell";
 
 export default function AdministratorLayout({children}:Readonly<{children:React.ReactNode}>){return <AdministratorShell>{children}</AdministratorShell>}
