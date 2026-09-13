7d75364e6805a9fde023adeb07a8a863085d9407
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 16:07:46 +0100

    Wire Early Years daily routines styles

--- web/app/headteacher/layout.tsx
+++ web/app/headteacher/layout.tsx
@@ -6,6 +6,7 @@ import "./headteacher-attendance.css";
 import "./headteacher-observations.css";
 import "./headteacher-planning.css";
 import "./headteacher-reports.css";
+import "./headteacher-routines.css";
 
 export default function HeadTeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
