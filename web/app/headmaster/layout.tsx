9dd7df6fa8802a92bef8e7e2eb080a6154d3ff82
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 15:20:57 +0100

    Wire Primary attendance styles into Headmaster layout

--- web/app/headmaster/layout.tsx
+++ web/app/headmaster/layout.tsx
@@ -3,6 +3,7 @@ import "./headmaster-assignments.css";
 import "./headmaster-teachers.css";
 import "./headmaster-pupils.css";
 import "./headmaster-academics.css";
+import "./headmaster-attendance.css";
 
 export default function HeadmasterLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
