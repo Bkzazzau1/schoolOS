e1ddb8262a1316bb918c2b6b9ac36c325e1496ee
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 14:06:38 +0100

    Load principal approvals styles

--- web/app/principal/layout.tsx
+++ web/app/principal/layout.tsx
@@ -1,4 +1,5 @@
 import "./principal.css";
+import "./principal-approvals.css";
 
 export default function PrincipalLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
