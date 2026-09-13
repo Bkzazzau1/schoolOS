389f7a8fc74016a9ec1f523ed5116deb95c56869
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-13 15:13:21 +0100

    Wire Primary teacher oversight styles

--- web/app/headmaster/layout.tsx
+++ web/app/headmaster/layout.tsx
@@ -1,5 +1,6 @@
 import "./headmaster.css";
 import "./headmaster-assignments.css";
+import "./headmaster-teachers.css";
 
 export default function HeadmasterLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
