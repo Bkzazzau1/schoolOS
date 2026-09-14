bea7ea7cfa476f5e62aa70aa41ab883f7fa1bbc5
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-14 01:03:59 +0100

    Load learning progress styles in teacher portal

--- web/app/teacher/layout.tsx
+++ web/app/teacher/layout.tsx
@@ -3,6 +3,7 @@ import "./module-pages.css";
 import "./reports.css";
 import "./weekly-progress/weekly-progress.css";
 import "./cbt/cbt.css";
+import "./learning-progress/learning-progress.css";
 
 export default function TeacherLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
